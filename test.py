d36075938ccb0abb5b9caa8d1ba2e2e809bd2751
Szhifan <sunzhifan233@gmail.com>
2025-06-17 20:28:46 +0200

    add grasp

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import torch 
-a = [(1,2,3), (4,5,6), (7,8,9)]
-b = torch.tensor(a)
-print(b)
+a = torch.randn(1, 100, 768)
+b = torch.randn(1, 100, 768)
+window = 10
+c = torch.zeros((1, 100, 100))
+for i in range(100):
+    start = max(0, i - window)
+    end = min(100, i + window)
+    c[:, i, start:end] = torch.bmm(a[:, i:i+1, :], b[:, start:end, :].transpose(1, 2)).squeeze(1)
+
+for i in c[0]:
+    print(i)
+    
